01227364860baa753280cf9251bb226f046de9c7
Esteban Russo <oerusso28@gmail.com>
2025-07-17 16:00:03 -0300

    new(react-learning):aprendiendo bases de react

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,11 @@
+// export function HelloWorldApp() {
+//     return <div>Hello World!!!!</div>;
+//   }
+
+// import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <div>HelloWorldApp</div>
+  )
+}
